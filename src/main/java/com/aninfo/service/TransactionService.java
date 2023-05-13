155e36437dd8226ad49c0137ad0240d21cf8d3e0
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2023-05-13 20:47:14 -0300

    remove unnecessary imports

--- src/main/java/com/aninfo/service/TransactionService.java
+++ src/main/java/com/aninfo/service/TransactionService.java
@@ -1,23 +1,14 @@
 package com.aninfo.service;
 
-import com.aninfo.exceptions.InsufficientFundsException;
-import com.aninfo.model.Account;
 import com.aninfo.model.Transaction;
-import com.aninfo.repository.AccountRepository;
 import com.aninfo.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class TransactionService {
-
-    @Autowired
-    private AccountRepository accountRepository;
 
     @Autowired
     private TransactionRepository transactionRepository;
