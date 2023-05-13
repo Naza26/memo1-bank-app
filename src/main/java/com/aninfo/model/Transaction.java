d9003eae1e40d7efddc10217f78f679b32f0ccce
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2023-05-13 20:45:13 -0300

    add getters and setters to visualize model in swagger + add transactionId attribute

--- src/main/java/com/aninfo/model/Transaction.java
+++ src/main/java/com/aninfo/model/Transaction.java
@@ -1,26 +1,49 @@
 package com.aninfo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long transactionId;
     private Long cbu;
     private TransactionType type;
     private Double amount;
 
-    public Transaction() {
-    }
+    public Transaction() {}
 
     public Transaction(Long cbu, TransactionType type, Double amount) {
         this.cbu = cbu;
         this.type = type;
         this.amount = amount;
+    }
+
+    public Long getTransactionId() {return transactionId;}
+
+    public Long getCbu() {
+        return cbu;
+    }
+
+    public void setCbu(Long cbu) {
+        this.cbu = cbu;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount( Double amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getTypeOfTransaction() {
+        return type;
+    }
+
+    public void setTransactionType( TransactionType type) {
+        this.type = type;
     }
 
     public enum TransactionType {
