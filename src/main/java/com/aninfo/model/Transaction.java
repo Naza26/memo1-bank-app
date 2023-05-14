4e2bbaeb913f8ac0b848c429d3f04af94479d58d
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2023-05-14 19:27:49 -0300

    add different generation type of id to avoid collision between accounts and transactions

--- src/main/java/com/aninfo/model/Transaction.java
+++ src/main/java/com/aninfo/model/Transaction.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 public class Transaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long transactionId;
     private Long cbu;
     private TransactionType type;
