7b730200f7f4b1b2b75a3772698de98ec04c6d3e
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2023-05-14 19:26:50 -0300

    add api to find all transactions from a specific account

--- src/main/java/com/aninfo/service/TransactionService.java
+++ src/main/java/com/aninfo/service/TransactionService.java
@@ -4,6 +4,8 @@ import com.aninfo.model.Transaction;
 import com.aninfo.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -23,6 +25,17 @@ public class TransactionService {
 
     public Optional<Transaction> findById(Long cbu) {
         return transactionRepository.findById(cbu);
+    }
+
+    public Collection<Transaction> findAllTransactionsByAccountId(Long cbu) {
+        Collection<Transaction> allTransactions = this.getTransactions();
+        Collection<Transaction> allTransactionsByGivenId = new ArrayList<>();
+        for (Transaction transaction : allTransactions) {
+           if (transaction.getCbu() == cbu) {
+               allTransactionsByGivenId.add(transaction);
+            }
+        }
+        return allTransactionsByGivenId;
     }
 
     public void deleteById(Long cbu) {
